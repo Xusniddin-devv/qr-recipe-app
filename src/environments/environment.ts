85f565b11d860399193a65b39306b678ce6536c6
XusniddinQalandarov <xusniddinqalandarov2004@gmail.com>
2025-06-09 11:18:50 +0500

    deleted env

--- src/environments/environment.ts
+++
@@ -1,6 +0,0 @@
-// filepath: src\environments\environment.ts
-export const environment = {
-  production: false,
-  openRouterApiKey:
-    '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',
-};
